f81afb5d7b60cc6a3c699fb369c27857e95b6a09
bruce1408 <summer56567@163.com>
2026-05-13 23:57:40 +0800

    Update dataset defaults and save path in download_calib_dataset.py

--- tools/download_calib_dataset.py
+++ tools/download_calib_dataset.py
@@ -51,14 +51,15 @@ if __name__ == '__main__':
     parser.add_argument(
         '--dataset_name',
         type=str,
-        default=['pileval', 'c4', 'wikitext2', 'ptb', 'ultrachat'],
+        # default=['pileval', 'c4', 'wikitext2', 'ptb', 'ultrachat'],
+        default=['pileval', 'c4', 'wikitext2', 'ultrachat'],
         nargs='*',
     )
     
     parser.add_argument('--save_path', 
                         type=str, 
                         # required=True
-                        default="/home/bruce_ultra/workspace/quant_workspace/Quantizer-Tools/_outputs/calibration_dataset/llmc_calib")
+                        default="/home/bruce_ultra/workspace/datasets/calibration_dataset/llmc_calib")
     args = parser.parse_args()
     logger.info(f'args : {args}')
     download(args.dataset_name, args.save_path)
